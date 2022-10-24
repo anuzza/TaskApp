ff668a7484284c00f0c6f3b16c61c793e8d2c3b3
anuzza <anuja3580@gmail.com>
2022-10-23 21:54:02 -0500

    adding validations

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect(
   "mongodb+srv://anuzza:anuja@cluster0.nv5zq7l.mongodb.net/task-manager-api",
@@ -8,13 +9,48 @@ mongoose.connect(
 const User = mongoose.model("User", {
   name: {
     type: String,
+    required: true,
+    trim: true,
   },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error(`Password cannot contain "password"`);
+      }
+    },
+  },
+
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
   },
 });
 
-const me = new User({ name: "Anuja", age: 19 });
+// const me = new User({
+//   name: "   gavin  ",
+//   email: "   gv@gmail.com  ",
+//   password: "ga",
+// });
 
 // me.save()
 //   .then(() => {
@@ -27,15 +63,19 @@ const me = new User({ name: "Anuja", age: 19 });
 const Task = new mongoose.model("Task", {
   description: {
     type: String,
+    required: true,
+    trim: true,
   },
   completed: {
     type: Boolean,
+    default: false,
   },
 });
 
-const task = new Task({ description: "Dry blanket", completed: false });
+const task = new Task({ description: "do quiz" });
 
-task.save()
+task
+  .save()
   .then(() => {
     console.log(task);
   })
