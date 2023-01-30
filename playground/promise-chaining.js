bfce33390b775255540083a2acd4ecb3ee027a9b
anuzza <anuja3580@gmail.com>
2023-01-30 17:52:14 -0600

    async await

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,6 +1,6 @@
 require("../src/db/mongoose");
 const User = require("../src/models/user");
- 
+
 User.findByIdAndUpdate("6355f9354f246aa919364192", { age: 1 })
   .then((user) => {
     console.log(user);
@@ -8,6 +8,20 @@ User.findByIdAndUpdate("6355f9354f246aa919364192", { age: 1 })
   })
   .then((result) => {
     console.log(result);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeAndCount("6355f9354f246aa919364192", 2)
+  .then((count) => {
+    console.log(count);
   })
   .catch((e) => {
     console.log(e);
--- playground/promise2.js
+++ playground/promise2.js
@@ -11,3 +11,17 @@ Task.findByIdAndRemove("6355fe2e89b511dde0bc50e4")
   .catch((e) => {
     console.log(e);
   });
+
+const deleteTaskAndCount = async (id) => {
+  await Task.findByIdAndRemove(id);
+  const count = Task.countDocuments({ compeleted: false });
+  return count;
+};
+
+deleteTaskAndCount("6355fe2e89b511dde0bc50e4")
+  .then((count) => {
+    console.log(count);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
