99c346f0e21870f79b9d67adbe4c7dd2cf88ac67
anuzza <anuja3580@gmail.com>
2023-02-11 21:49:19 -0600

    express middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send("GET req are disabled");
+//   } else {
+//     next();
+//   }
+// });
+
+// app.use((req, res, next) => {
+//   res.status(503).send("site under maintenance. will get back to you soon!");
+// });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -13,6 +25,9 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
+
+//without middleware: new req-> run route handler
+//with middleware: new req-> do something -> run route handler
 
 // const jwt = require("jsonwebtoken");
 
