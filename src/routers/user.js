5130e89b3da2a9a548c26187bb86628398501448
anuzza <anuja3580@gmail.com>
2023-02-12 17:12:33 -0600

    updating user profile

--- src/routers/user.js
+++ src/routers/user.js
@@ -69,7 +69,7 @@ router.get("/users/:id", async (req, res) => {
   }
 });
 
-router.patch("/users/:id", async (req, res) => {
+router.patch("/users/me", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["name", "email", "password", "age"];
   const isValid = updates.every((update) => allowedUpdates.includes(update));
@@ -79,19 +79,19 @@ router.patch("/users/:id", async (req, res) => {
   }
 
   try {
-    const user = await User.findById(req.params.id);
+    // const user = await User.findById(req.params.id);
 
-    updates.forEach((update) => (user[update] = req.body[update]));
+    updates.forEach((update) => (req.user[update] = req.body[update]));
 
-    await user.save();
+    await req.user.save();
 
-    if (!user) {
-      return res.status(404).send();
-    }
+    // if (!user) {
+    //   return res.status(404).send();
+    // }
 
-    res.send(user);
+    res.send(req.user);
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).send(error.message);
   }
 });
 
