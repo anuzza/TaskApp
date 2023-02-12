90db269a5c4d927ef105adcf1dfcf200cb5d228a
anuzza <anuja3580@gmail.com>
2023-02-12 16:59:43 -0600

    authenticating user endpoints

--- src/index.js
+++ src/index.js
@@ -29,3 +29,13 @@ app.listen(port, () => {
 // };
 
 // myFunction();
+
+// const pet = {
+//   name: "Hal",
+// };
+
+// pet.toJSON = function () {
+//   console.log(this);
+//   return this;
+// };
+// console.log(JSON.stringify(pet));
