07de72df6345d356140b458ac00730f6a82c466c
anuzza <anuja3580@gmail.com>
2023-02-11 22:06:29 -0600

    accepting auth tokens

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = new express.Router();
+const auth = require("../middleware/auth");
 const User = require("../models/user");
 
 router.post("/users", async (req, res) => {
@@ -27,13 +28,8 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get("/users/:id", async (req, res) => {
