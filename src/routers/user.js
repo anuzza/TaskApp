cb7800816a349cac718c3b183bb2a0620fc04265
anuzza <anuja3580@gmail.com>
2023-02-12 16:42:21 -0600

    logout user

--- src/routers/user.js
+++ src/routers/user.js
@@ -28,6 +28,28 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(
+      (token) => token.token != req.token
+    );
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.status(200).send();
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
